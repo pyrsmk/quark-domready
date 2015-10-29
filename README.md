e23948db4b774f10f87a0e8d949c1e005a32eea1
pyrsmk <dev@dreamysource.fr>
2015-10-29 17:18:35 +0100

    Update build

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-quark-domready 0.1.0
+quark-domready 0.1.1
 ====================
 
 [dom-ready](https://github.com/noordawod/dom-ready) module for [quark](https://github.com/pyrsmk/quark).
--- src/quark-domready.js
+++ src/quark-domready.js
@@ -1,3 +1,3 @@
-/*! quark-domready 0.1.0 (https://github.com/pyrsmk/quark-domready) */
+/*! quark-domready 0.1.1 (https://github.com/pyrsmk/quark-domready) */
 
 quark.$._whenReady = domReady;
